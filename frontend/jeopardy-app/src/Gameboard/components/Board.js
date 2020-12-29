8a26b79638a9da0cba416e820fe779165b301821
Drew <drewjw10@gmail.com>
2020-12-28 21:24:38 -0800

    game board formatting enhancements

--- frontend/jeopardy-app/src/Gameboard/components/Board.js
+++ frontend/jeopardy-app/src/Gameboard/components/Board.js
@@ -1,31 +1,29 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
 import BoardTile from "./BoardTile";
+import "./Board.css";
 
 const Board = () => {
-    
-    const ROWS = 5;
-    const COLUMNS = 5;
-    const BOARD = [];
-    const createTable = () => {
-        for (let i = 0; i < ROWS; i++) {
-            let children = []
-            for (let j = 0; j < COLUMNS; j++) {
-                children.push(<BoardTile content={j}/>)
-            }
-            BOARD.push(children)
-        }
-        return BOARD;
+  const ROWS = 5;
+  const COLUMNS = 5;
+  const BOARD = [];
+  const createTable = () => {
+    for (let i = 0; i < ROWS; i++) {
+      let children = [];
+      for (let j = 0; j < COLUMNS; j++) {
+        children.push(<BoardTile content={j} />);
+      }
+      BOARD.push(children);
     }
+    return BOARD;
+  };
 
-    return (
-      <div id="board-table">
-        <h2>Jeopardy game board</h2>
-            <div className="board-tiles">
-                {createTable()}
-            </div>
-      </div>  
-    )
-}
+  return (
+    <div id='board-table'>
+      <h2>Jeopardy game board</h2>
+      <div className='board-tiles'>{createTable()}</div>
+    </div>
+  );
+};
 
 export default Board;
--- frontend/jeopardy-app/src/Gameboard/components/BoardTile.js
+++ frontend/jeopardy-app/src/Gameboard/components/BoardTile.js
@@ -2,14 +2,12 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 
 const BoardTile = (props) => {
-    const { content } = props;
-    return (
-    <Card style={{ width: '5rem' }}>
-        <Card.Body>
-            {content} Hello!
-        </Card.Body>
+  const { content } = props;
+  return (
+    <Card style={{ width: "8rem", height: "8rem" }}>
+      <Card.Body>{content} Hello!</Card.Body>
     </Card>
-    );
-}
+  );
+};
 
 export default BoardTile;
